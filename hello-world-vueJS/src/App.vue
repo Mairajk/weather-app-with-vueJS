6dfe09a86b946d41f99d7ce1375792ba30d1d426
Mairajk <mairajkhan597@gmail.com>
2023-01-16 18:13:25 +0500

    practice 1

---
+++ hello-world-vueJS/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+<div class="main">
+
+    <form action="" v-on:submit="getWeather(e)">
+        <input type="text" placeholder="Enter the city name" @v-model="inputCity">
+
+        <button type="submit">Submit</button>
+    </form>
+
+    <!-- <Weather :city="`${inputCity}`" /> -->
+
+</div>
+</template>
+
+<script>
+import Weather from './components/Weather.vue'
+
+export default {
+    name: 'App',
+    components: {
+        Weather
+    },
+
+    data() {
+        return {
+            weatherData: []
+        }
+    },
+
+    methods: {
+
+        getWeather(e) {
+            // e.preventDefault();
+
+            try {
+                const data = axios.get(`https://api.weatherapi.com/v1/current.json?key=ac66a8ffaa8e4d219fd120436231601&q=${this.city}&aqi=no`);
+                this.weatherData = data;
+
+                console.log('city ===>', this.city);
+                console.log('data ===>', this.weatherData);
+
+            } catch (error) {
+                console.log('error ===> ', error);
+            }
+        }
+
+    }
+}
+</script>
---
+++ hello-world-vueJS/src/components/Weather.vue
@@ -0,0 +1,26 @@
+<template>
+<div class="postForm">
+
+</div>
+</template>
+
+<script>
+import axios from 'axios';
+import Weather from './Weather.vue';
+
+export default {
+    name: 'Weather',
+    components: {
+
+    },
+
+    data() {
+        return {
+            // weatherData: []
+        }
+    },
+
+}
+</script>
+
+///////////////  ac66a8ffaa8e4d219fd120436231601
