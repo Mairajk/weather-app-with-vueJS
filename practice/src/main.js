16852bbdea6c81472d219eb5bdc2f04f4586b6de
Mairajk <mairajkhan597@gmail.com>
2023-01-17 18:42:35 +0500

    add another folder...5

--- practice/src/main.js
+++ practice/src/main.js
@@ -1,4 +1,37 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import { mutations, actions, getters, createStore } from "vuex";
+import axios from "axios";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+const store = createStore({
+  state() {
+    return {
+      a: 0,
+      data: "",
+    };
+  },
+
+  mutations: {
+    increment(state) {
+      ++state.a;
+    },
+    setData(state, d) {
+      state.data = d;
+    },
+  },
+  actions: {
+    async callApi(context) {
+      let response = await axios.get(
+        "https://api.weatherapi.com/v1/current.json?key=bf30ea59efc941f5bf055856231701&q=karachi&aqi=no"
+      );
+
+      context.commit();
+    },
+  },
+});
+
+const app = createApp(App);
+
+app.use(store);
+
+app.mount("#app");
